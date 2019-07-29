c59f6aef635f48edaa3835031a94f8b1064d3140
Shaili Jha <shaili_jha@yahoo.co.in>
2019-07-28 23:39:19 -0400

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://shaili123:shaili123@cluster0-btd7m.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
