3071760a572c4a75ca9342a50e39c76eb229f45e
shailijha <shaili_jha@yahoo.co.in>
2019-08-15 19:22:12 -0400

    Updated dev connector

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+const PostItem = ({
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  auth
+}) => {
+  return <div />;
+};
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(PostItem);
