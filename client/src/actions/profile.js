46fa4d632132f3b24ebe99645cf80fd7d787d25a
shailijha <shaili_jha@yahoo.co.in>
2019-08-14 23:13:43 -0400

    functions added for getProfiles, getProfileById and getGithubRepos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,8 @@ import {
   UPDATE_PROFILE,
   ACCOUNT_DELETED,
   CLEAR_PROFILE,
-  GET_PROFILES
+  GET_PROFILES,
+  GET_REPOS
 } from './types';
 
 // Get the current users profile
@@ -26,13 +27,49 @@ export const getCurrentProfile = () => async dispatch => {
   }
 };
 
-// Get all profiles
+// Get all profiles by making a get request to /api/profile
 export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
   try {
     const res = await axios.get('/api/profile');
 
     dispatch({
       type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get all profiles by making a get request to /api/profile/user/userId
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
       payload: res.data
     });
   } catch (err) {
@@ -190,7 +227,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone')) {
     try {
-      const res = await axios.delete(`/api/profile/`);
+      await axios.delete(`/api/profile/`);
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
