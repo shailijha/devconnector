61d66890900f6cc5e26c7dd9fdf8eac819ddae25
shailijha <shaili_jha@yahoo.co.in>
2019-08-16 17:26:42 -0400

    Posts.js created
    Initially fetches the posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,26 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
-import PostItem from './PostItem';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { post, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return loading ? (
-    <Spinner />
-  ) : (
-    <Fragment>
-      <h1 className='large text-primary'>Posts</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome to the community
-      </p>
-      {/* PostForm */}
-      {posts.map(post => (
-        <PostItem key={post._id} post={post} />
-      ))}
-    </Fragment>
-  );
+
+  return <div />;
 };
 
 Posts.propTypes = {
