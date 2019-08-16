1d7d916f3a99b0e92e3bc9de5d0d80393f414fd7
shailijha <shaili_jha@yahoo.co.in>
2019-08-16 17:26:07 -0400

    function to get posts is added to actions file

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_POSTS, POST_ERROR } from './types';
 
-// function to get posts
+// Function to get posts
 export const getPosts = () => async dispatch => {
   try {
     const res = await axios.get('/api/posts');
