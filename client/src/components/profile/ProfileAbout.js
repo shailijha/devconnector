36f18ee2bc6b4e10187409af637cc8444722e66d
shailijha <shaili_jha@yahoo.co.in>
2019-08-17 12:50:53 -0400

    Updated ProfileAbout.js
    Added icons for skill set lingo

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -8,6 +8,17 @@ const ProfileAbout = ({
     user: { name }
   }
 }) => {
+  const mapLanguageIcons = new Map([
+    ['html', <i class='fab fa-html5 fa-2x' />],
+    ['php', <i class='fab fa-php fa-2x' />],
+    ['java', <i class='fab fa-java fa-2x' />],
+    ['javascript', <i class='fab fa-js-square fa-2x' />],
+    ['js', <i class='fab fa-js-square fa-2x' />],
+    ['react', <i class='fab fa-react fa-2x' />],
+    ['css', <i class='fab fa-css3-alt fa-2x' />],
+    ['python', <i class='fab fa-python fa-2x' />]
+  ]);
+
   return (
     <div className='profile-about bg-light p-2'>
       {bio && (
@@ -21,7 +32,12 @@ const ProfileAbout = ({
       <div className='skills'>
         {skills.map((skill, index) => (
           <div key={index} className='p-1'>
-            <i class='fa fa-check' /> {skill}
+            {mapLanguageIcons.has(skill.toLowerCase()) ? (
+              mapLanguageIcons.get(skill.toLowerCase())
+            ) : (
+              <i class='fa fa-check' />
+            )}
+            {skill}
           </div>
         ))}
       </div>
