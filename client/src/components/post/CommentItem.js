d7ea220c56c470f62f5e748805d15949e3f17251
shailijha <shaili_jha@yahoo.co.in>
2019-08-17 21:55:29 -0400

    updated comment item to include comment form

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,56 +1,73 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
-import { deleteComment, updateComment } from '../../actions/post';
+import { deleteComment } from '../../actions/post';
+import UpdateCommentForm from './UpdateCommentForm';
 
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
   auth,
   deleteComment
-}) => (
-  <div class='post bg-white p-1 my-1'>
-    <div>
-      <Link to={`/profile/${user}`}>
-        <img class='round-image' src={avatar} alt='' />
-        <h4>{name}</h4>
-      </Link>
+}) => {
+  const [commentForm, toggleCommentForm] = useState(false);
+
+  return (
+    <div class='post bg-white p-1 my-1'>
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img class='round-image' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p class='my-1'>{text}</p>
+        <p class='post-date'>
+          Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+        </p>
+        {!auth.loading && user === auth.user._id && (
+          <Fragment>
+            <button
+              onClick={e => deleteComment(postId, _id)}
+              type='button'
+              className='btn btn-danger'
+            >
+              <i className='fas fa-times' />
+            </button>
+            <button
+              onClick={e => toggleCommentForm(!commentForm)}
+              type='button'
+              className='btn'
+            >
+              <i className='far fa-edit' />
+            </button>{' '}
+            {commentForm ? (
+              <Fragment>
+                <br />
+                <br />
+                <UpdateCommentForm
+                  postId={postId}
+                  commentId={_id}
+                  commenttext={text}
+                />
+              </Fragment>
+            ) : (
+              ' '
+            )}
+          </Fragment>
+        )}
+      </div>
     </div>
-    <div>
-      <p class='my-1'>{text}</p>
-      <p class='post-date'>
-        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
-      </p>
-      {!auth.loading && user === auth.user._id && (
-        <Fragment>
-          <button
-            onClick={e => deleteComment(postId, _id)}
-            type='button'
-            className='btn btn-danger'
-          >
-            <i className='fas fa-times' />
-          </button>
-          <button
-            onClick={e => updateComment(postId, _id, text)}
-            type='button'
-            className='btn'
-          >
-            <i className='far fa-edit' />
-          </button>
-        </Fragment>
-      )}
-    </div>
-  </div>
-);
+  );
+};
 
 CommentItem.propTypes = {
   auth: PropTypes.object.isRequired,
   postId: PropTypes.number.isRequired,
   comment: PropTypes.object.isRequired,
-  deleteComment: PropTypes.func.isRequired,
-  updateComment: PropTypes.func.isRequired
+  deleteComment: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -59,5 +76,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { deleteComment, updateComment }
+  { deleteComment }
 )(CommentItem);
