94dae47063efc2a8dc948d35eb1f3937820f0b88
shailijha <shaili_jha@yahoo.co.in>
2019-08-19 10:07:19 -0400

    Update ProfileGithub.js

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
