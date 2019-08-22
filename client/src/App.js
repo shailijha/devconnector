375c4163ebcb2bf6a6e0e581d175bcfc4ebc2f0f
shailijha <shaili_jha@yahoo.co.in>
2019-08-21 20:47:29 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ if (localStorage.token) {
 
 const App = () => {
   useEffect(() => {
-    document.title = 'Welcome to Geekbook';
+    //document.title = 'Welcome to Geekbook';
     store.dispatch(loadUser());
   }, []);
   return (
